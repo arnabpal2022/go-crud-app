d4331f82ad27995242e94158b2cd051eeffec725
Arnab Pal <arnabpalofficial2003@gmail.com>
2024-06-04 16:11:18 +0530

    feat: updated project with gin framework

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
+	"github.com/gin-gonic/gin"
 )
 
-// We're not using a Database in Case of This, So we'll be going through making a Struct 
+// We're not using a Database in Case of This, So we'll be going through making a Struct
 // that will be encoded in json while making API Requests
 
 type Movie struct {
@@ -27,82 +26,81 @@ type Director struct {
 
 var movies []Movie
 
-// The function `getallmovies` sets the response content type to JSON and encodes a list of movies to
-// the response writer.
-func getallmovies(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(movies)
+// The function `getallmovies` in Go returns all movies in JSON format using the Gin framework.
+func getallmovies(c *gin.Context){
+	c.JSON(http.StatusOK, movies)
 }
 
-// The function `deletemovie` deletes a movie from a list of movies based on the provided ID.
-func deletemovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // Vars returns the route variables for the current request, if any.
+// The `deletemovie` function in Go deletes a movie from a collection based on its ID and returns the
+// updated collection.
+func deletemovie(c *gin.Context) {
+	id := c.Param("id") // Param returns the value of the URL param.
 	for index, item := range movies {
-		if item.ID == params["id"] {
-			movies = append(movies[:index], movies[index+1:]...) // Used to Delete an Item
+		if item.ID == id {
+			movies = append(movies[:index], movies[index+1:]...) // Delete Logic
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(movies)
+	c.JSON(http.StatusOK, movies)
 }
 
-// The function retrieves a specific movie by its ID and returns it as JSON in a HTTP response.
-func getmovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // Vars returns the route variables for the current request, if any.
-	// Showing only the "id"th item
-	for _, item := range movies {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
+// The function `getmovie` retrieves a movie by its ID and returns it as JSON, or a "movie not found"
+// error if the movie is not found.
+func getmovie(c *gin.Context) {
+    id := c.Param("id")
+    for _, item := range movies {
+        if item.ID == id {
+            c.JSON(http.StatusOK, item)
+            return
+        }
+    }
+    c.JSON(http.StatusNotFound, gin.H{"error": "movie not found"})
 }
 
-// The function `createmovie` decodes a JSON request body into a Movie struct, assigns a random ID to
-// the movie, adds it to a slice of movies, and then encodes and returns the movie as JSON in the
-// response.
-func createmovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+// The function `createmovie` in Go creates a new movie object from JSON input, assigns a random ID,
+// and adds it to a list of movies.
+func createmovie(c *gin.Context) {
 	var movie Movie
-	_ = json.NewDecoder(r.Body).Decode(&movie)
+	if err := c.ShouldBindJSON(&movie); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
 	movie.ID = strconv.Itoa(rand.Intn(10000000)) // Random ID Generation
 	movies = append(movies, movie)
-	json.NewEncoder(w).Encode(movie)
+	c.JSON(http.StatusCreated, movie)
 }
 
-// The function `updatemovie` updates a movie in a list based on the provided ID in a Go web
-// application.
-func updatemovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
+// The function `updatemovie` updates a movie in a collection based on the provided ID.
+func updatemovie(c *gin.Context) {
+    id := c.Param("id") // Param returns the value of the URL param.
+    var updatedMovie Movie
+    if err := c.ShouldBindJSON(&updatedMovie); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
 
-	// Logic is Delete that record and Create a Record, Just the ID is Same.
-	for index, item := range movies {
-		if item.ID == params["id"] {
-			movies = append(movies[:index], movies[index+1:]...)
-			var movie Movie
-			_ = json.NewDecoder(r.Body).Decode(&movie)
-			movie.ID = params["id"]
-			movies = append(movies, movie)
-			json.NewEncoder(w).Encode(movie)
-		}
-	}
+    for index, item := range movies {
+        if item.ID == id {
+            movies[index] = updatedMovie
+            movies[index].ID = id // Preserve original ID
+            c.JSON(http.StatusOK, movies[index])
+            return
+        }
+    }
+    c.JSON(http.StatusNotFound, gin.H{"error": "movie not found"})
 }
 
 func main() {
-	r := mux.NewRouter()
+	r := gin.Default()
 
 	movies = append(movies, Movie{ID: "1", ISBN: "100001", Name: "Movie One", Director: &Director{FirstName: "John", LastName: "Doe"}})
 	movies = append(movies, Movie{ID: "2", ISBN: "100002", Name: "Movie Two", Director: &Director{FirstName: "John", LastName: "K"}})
 
-	r.HandleFunc("/movies", getallmovies).Methods("GET")
-	r.HandleFunc("/movies/{id}", getmovie).Methods("GET")
-	r.HandleFunc("/movies", createmovie).Methods("POST")
-	r.HandleFunc("/movies/{id}", updatemovie).Methods("PUT")
-	r.HandleFunc("/movies/{id}", deletemovie).Methods("DELETE")
+	r.GET("/movies", getallmovies)
+	r.GET("/movies/:id", getmovie)
+	r.POST("/movies", createmovie)
+	r.PUT("/movies/:id", updatemovie)
+	r.DELETE("/movies/:id", deletemovie)
 
 	fmt.Printf("Starting Server at Port 8000\n")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(r.Run(":8080"))
 }
